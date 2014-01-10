7989409ea3b316eb2dda7318d3239425f77310bf
Gaurav Soni <raveformails@gmail.com>
2014-01-10 19:44:34 +0530

    cricketism - add send grid account

--- config/environment.rb
+++ config/environment.rb
@@ -9,3 +9,13 @@ my_date_formats = { :default => '%d-%b-%Y' } #or any other you like
 
 Time::DATE_FORMATS.merge!(my_datetime_formats)
 Date::DATE_FORMATS.merge!(my_date_formats)
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
